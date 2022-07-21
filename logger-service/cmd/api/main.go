28efc9157e3314e3b7bd595bf32411483b1dca80
Rob Dale <rob@robdale.me.uk>
2022-07-21 18:51:16 +0100

    Started implementing rpc in logging-service

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"github.com/robdale110/go-microservices/logger-service/data"
@@ -48,8 +49,11 @@ func main() {
 		Models: data.New(client),
 	}
 
+	// register the RPC server
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
+
 	// start web server
-	//go app.serve()
 	log.Println("Starting service on port", webPort)
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -62,17 +66,23 @@ func main() {
 	}
 }
 
-// func (app *Config) serve() {
-// 	srv := &http.Server{
-// 		Addr:    fmt.Sprintf(":%s", webPort),
-// 		Handler: app.routes(),
-// 	}
+func (app *Config) rpcListen() {
+	log.Println("Starting RPC server on port ", rpcPort)
+	liste, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
 
-// 	err := srv.ListenAndServe()
-// 	if err != nil {
-// 		log.Panic()
-// 	}
-// }
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+
+	}
+}
 
 func connectToMongo() (*mongo.Client, error) {
 	// create connection options
