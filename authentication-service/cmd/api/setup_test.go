133bde421102ac2e4a225d8fbc611559463e6ca0
Rob Dale <rob@robdale.me.uk>
2022-08-05 18:40:26 +0100

    Finished testing authentication service

--- authentication-service/cmd/api/setup_test.go
+++ authentication-service/cmd/api/setup_test.go
@@ -3,9 +3,14 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/robdale110/go-microservices/authentication-service/data"
 )
 
+var testApp Config
+
 func TestMain(m *testing.M) {
-	
+	repo := data.NewPostgresTestRepository(nil)
+	testApp.Repo = repo
 	os.Exit(m.Run())
 }
